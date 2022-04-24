ebc35068fd6b4d51046f970cd66765d9f5c71b9a
Roger9 <gutierrez.rog12@gmail.com>
2022-04-24 12:46:00 -0300

    Project init

---
+++ src/styles/global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
