3fbc97efe251297555d0d0b579f183a0b10de51e
Roger9 <gutierrez.rog12@gmail.com>
2022-05-02 11:56:50 -0300

    Lazy import implemented
        Loader created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import { Suspense, lazy } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { LOGIN, REGISTER } from 'routes'
 import { AnimatePresence } from 'framer-motion'
-import Login from 'components/views/forms/Login'
-import Register from 'components/views/forms/Register'
+import Loader from 'components/Loader'
+
+const Login = lazy(() => import('components/views/forms/Login'))
+const Register = lazy(() => import('components/views/forms/Register'))
 
 export const App = () => {
   const location = useLocation()
 
   return (
-    <AnimatePresence exitBeforeEnter>
-      <Routes location={location} key={location.pathname}>
-        <Route path={LOGIN} element={<Login />} />
-        <Route path={REGISTER} element={<Register />} />
-      </Routes>
-    </AnimatePresence>
+    <Suspense fallback={<Loader />}>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path={LOGIN} element={<Login />} />
+          <Route path={REGISTER} element={<Register />} />
+        </Routes>
+      </AnimatePresence>
+    </Suspense>
   )
 }
---
+++ src/components/Loader/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const StyledLoader = styled.span`
+  width: 2.4rem;
+  height: 2.4rem;
+  border: 3px solid var(--shadow);
+  border-radius: 50%;
+  border-top-color: var(--orange);
+  animation: spin 1s ease-in-out infinite;
+  margin: auto;
+
+  @keyframes spin {
+    to { transform: rotate(360deg); }
+  }
+`
